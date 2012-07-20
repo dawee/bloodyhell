3828a26950427a874594975f8cf87d3af1fe5922
david <david@mediaodyssee.com>
2012-07-20 10:14:01 +0200

    correct sample path

--- tests/bloodyhell/sample.py
+++ tests/bloodyhell/sample.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..', '..', 'src')
+    os.path.join(os.path.dirname(__file__), '..', '..')
 ))
 
 from bloodyhell.game import Game
